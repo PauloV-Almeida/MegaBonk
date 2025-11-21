35dfe72e8b974bb5abe8c8114d0540e6e9ac81a9
pkagoto <155785094+pkagoto@users.noreply.github.com>
2025-11-21 20:29:47 -0300

    goblin

---
+++ src/Goblin.cpp
@@ -0,0 +1,81 @@
+#include "../include/Goblin.h"
+
+namespace Entidades
+{
+	namespace Personagens
+	{
+
+		Goblin::Goblin(bool viv, int nV, sf::Vector2f pos, sf::Vector2f velo, float dmg, sf::Vector2f tam, float fur):
+			Inimigo(1, pos, velo, tam),
+			furia(fur)
+		{
+			dano = dmg;
+			n_vidas = nV;
+			texturas = pGG->carregar_texturas("./assets/Esqueleto.png");
+			corpo.setTexture(texturas);
+			vivo = viv;
+
+		}
+		Goblin::~Goblin()
+		{
+		}
+		void Goblin::executar()
+		{
+			mover();
+			desenhar();
+		}
+		void Goblin::danificar()
+		{
+			if (velocidade > 0)
+			{
+				vel.y -= 5.f;
+				corpo.setPosition(corpo.getPosition() + sf::Vector2f(velocidade * vel.x / 10, velocidade * vel.y / 10));
+			}
+
+
+		}
+		void Goblin::colidir(Jogador* pJog, std::string direcao)
+		{
+			pJog->receber_dano(dano);
+			if (direcao == "Embaixo")
+			{
+				noChao = true;
+				vel.y = 0.0f;
+			}
+			else if (direcao == "Cima")
+			{
+				vel.y = 0.0f;
+			}
+			else if (direcao == "Esquerda")
+			{
+				vel.x = 0.0f;
+			}
+			else if (direcao == "Direita")
+			{
+				vel.x = 0.0f;
+			}
+			dano *= furia;
+		}
+		void Goblin::colidir(Obstaculos::Obstaculo* pObs, std::string direcao)
+		{
+			if (direcao == "Embaixo")
+			{
+				noChao = true;
+				vel.y = 0.0f;
+			}
+			else if (direcao == "Cima")
+			{
+				vel.y = 0.0f;
+			}
+			else if (direcao == "Esquerda" || direcao == "Direita")
+			{
+				vel.x = 0.0f;
+			}
+		}
+
+		void Goblin::desenhar()
+		{
+			pGG->desenhar(&corpo);
+		}
+	}
+}
